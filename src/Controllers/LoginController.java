8e3d6d673019d408072e0ebb7c6fe3e9b43ce4c9
Dexter <vzapata311@gmail.com>
2022-12-07 23:05:01 -0400

    Initializing project

---
+++ src/Controllers/LoginController.java
@@ -0,0 +1,14 @@
+package Controllers;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Dexter
+ */
+public class LoginController {
+    
+}
